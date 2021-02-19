5f77e18307efef75b9314578a34441d47cff32d8
Igor Buldashov <meter@mail.ru>
2021-02-19 20:53:12 +0500

    #20 - установка Docker плейбуком

--- gitlab-ci/terraform/main.tf
+++ gitlab-ci/terraform/main.tf
@@ -6,21 +6,28 @@ provider "yandex" {
   version = "~> 0.35.0"
 }
 
-resource "yandex_compute_instance" "docker" {
-   name                      = "docker-app-${count.index}"
+data "yandex_compute_image" "my_image" {
+  family = "ubuntu-1804-lts"
+}
+
+
+resource "yandex_compute_instance" "gitlab" {
+   name                      = "gitlab-ci-${count.index}"
    platform_id               = "standard-v2"
    count                     = var.count_app
 
   resources {
     cores  = 2
-    memory = 2
-    core_fraction = 20
+    memory = 4
+    core_fraction = 100
   }
 
   boot_disk {
     initialize_params {
     # Указать id образа с Docker
-      image_id = var.image_id
+    #  image_id = var.image_id
+      image_id = "${data.yandex_compute_image.my_image.id}"
+      size = 50
     }
 
   }
@@ -29,7 +36,7 @@ resource "yandex_compute_instance" "docker" {
     nat       = true
   }
 
-  zone                     = var.zone
+  zone = var.zone
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
@@ -47,7 +54,7 @@ resource "yandex_compute_instance" "docker" {
    }
 
    provisioner "local-exec" {
-     command = "ansible-playbook --inventory ${self.network_interface.0.nat_ip_address}, run_app.yml"
+     command = "ansible-playbook --inventory ${self.network_interface.0.nat_ip_address}, install_docker.yml"
      working_dir = "../ansible"
    }
 
--- gitlab-ci/terraform/variables.tf
+++ gitlab-ci/terraform/variables.tf
@@ -13,9 +13,9 @@ variable public_key_path {
   # Описание переменной
   description = "Path to the public key used for ssh access"
 }
-variable image_id {
-  description = "Disk image"
-}
+#variable image_id {
+#  description = "Disk image"
+#}
 variable subnet_id {
   description = "Subnet"
 }
