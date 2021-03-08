f7b3f32bd1680fc520bc8ca6e98e56e7b813a9ae
Igor Buldashov <meter@mail.ru>
2021-03-08 16:32:06 +0500

    #22 - Основное задание

---
+++ monitoring/terraform/outputs.tf
@@ -0,0 +1,13 @@
+output "external_ip_address_app" {
+  value = yandex_compute_instance.monitoring.*.network_interface.0.nat_ip_address
+}
+
+#resource "local_file" "AnsibleInventory" {
+#  content = templatefile("inventory.tmpl",
+#    {
+#    name = yandex_compute_instance.gitlab.*.name
+#    extip = yandex_compute_instance.gitlab.*.network_interface.0.nat_ip_address
+#    }
+#  )
+#  filename = "../ansible/inventory"
+#}
